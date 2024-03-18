45c3d42c0512bc7a32feafc3480943145840edd5
Leonard Ehrenfried <mail@leonard.io>
2024-03-18 13:26:31 +0100

    Improve entry script]

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,12 @@ FROM docker.io/eclipse-temurin:21
 
 WORKDIR /photon/
 
+ENV PHOTON_LANGUAGES=de,en,fr
+
 ADD https://github.com/komoot/photon/releases/download/0.5.0/photon-0.5.0.jar /photon/photon.jar
 
-ENTRYPOINT entrypoint.sh
+COPY entrypoint.sh /photon/
+
+ENTRYPOINT /photon/entrypoint.sh
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,15 @@
 #!/bin/bash -e
 
-if [ "$1" = 'photon' ]; then
-  shift
+java -jar photon.jar \
+      -nominatim-import \
+      -host      $NOMINATIM_DB_HOST \
+      -port      $NOMINATIM_DB_PORT \
+      -user      $NOMINATIM_DB_USER \
+      -password  $NOMINATIM_DB_PASSWORD \
+      -languages $PHOTON_LANGUAGES
+echo
+echo 'Photon init process complete; ready for start up.'
+echo
 
-  java -jar photon.jar \
-        -nominatim-import \
-        -host      $NOMINATIM_DB_HOST \
-        -port      $NOMINATIM_DB_PORT \
-        -user      $NOMINATIM_DB_USER \
-        -password  $NOMINATIM_DB_PASSWORD \
-        -extra-tags ref:IFOPT \
-        -languages $PHOTON_LANGUAGES
-  echo
-  echo 'Photon init process complete; ready for start up.'
-  echo
+java -jar photon.jar -languages "$PHOTON_LANGUAGES" "$@"
 
-  java -jar photon.jar -extra-tags ref:IFOPT -languages "$PHOTON_LANGUAGES" "$@"
-
-fi
-
-exec "$@"
