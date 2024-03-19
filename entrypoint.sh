0c9c58dea567170a305c4950ec8283a2c8504fd9
Leonard Ehrenfried <mail@leonard.io>
2024-03-19 14:17:48 +0100

    Skip import if it already exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -e
 
-java -jar photon.jar \
-      -nominatim-import \
-      -host      $NOMINATIM_DB_HOST \
-      -port      $NOMINATIM_DB_PORT \
-      -user      $NOMINATIM_DB_USER \
-      -password  $NOMINATIM_DB_PASSWORD \
-      -languages $PHOTON_LANGUAGES
-echo
-echo 'Photon init process complete; ready for start up.'
-echo
+DATA_DIR="/photon/photon_data"
+
+if [ ! -d "$DATA_DIR" ]; then
+
+  echo "Photon data directory doesn't exist. Starting import."
+
+  java -jar photon.jar \
+        -nominatim-import \
+        -host      "$NOMINATIM_DB_HOST" \
+        -port      "$NOMINATIM_DB_PORT" \
+        -user      "$NOMINATIM_DB_USER" \
+        -password  "$NOMINATIM_DB_PASSWORD" \
+        -languages "$PHOTON_LANGUAGES"
+  echo
+  echo 'Photon init process complete; ready for start up.'
+  echo
+fi
 
 java -jar photon.jar -languages "$PHOTON_LANGUAGES" "$@"
 
