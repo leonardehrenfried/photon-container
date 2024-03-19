38323ba5ea895345667d2466a2572d6987e233f6
Leonard Ehrenfried <mail@leonard.io>
2024-03-19 15:10:57 +0100

    Move command to end

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 DATA_DIR="/photon"
 java -jar photon.jar \
-        -nominatim-update \
         -host      "$NOMINATIM_DB_HOST" \
         -port      "$NOMINATIM_DB_PORT" \
         -user      "$NOMINATIM_DB_USER" \
         -password  "$NOMINATIM_DB_PASSWORD" \
-        -data-dir  "$DATA_DIR"
+        -data-dir  "$DATA_DIR" \
+        -nominatim-update
